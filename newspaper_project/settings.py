deb1e5fba8df716c551071dc8f8dedf55b689b97
Andrey Bolshakov <abolshakovy@gmail.com>
2020-01-21 23:04:28 +0300

    Add reset password and email leter

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -124,3 +124,9 @@ LOGOUT_REDIRECT_URL = 'home'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = 'Macjus'
+EMAIL_HOST_PASSWORD = 'Agava1974'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
